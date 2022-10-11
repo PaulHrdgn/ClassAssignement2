aa59b77baac699880ce09aa2a45b36eee13fa174
Paul Herdegen <herdegenpaul3@gmail.com>
2022-10-11 05:55:04 +0200

    commit with API

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const port = 3000;
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('C:\Users\Paul\Desktop\Assignment 2 WebProg\ClassAssignement2\public\index.html');
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-})
+app.get('/api/burger', (req, res) => {
+    console.log(req.query)
+    if (req.query.value== 'BigMac'){
+        res.send([{Nom:"Steak"},{Nom:"Salade"},{Nom:"Fromage" },{Nom:"Pain" }])
+    }
+    if (req.query.value== 'TripleCheese'){
+        res.send([{Nom:"Steak"},{Nom:"Fromage"},{Nom:"Cornichons" },{Nom:"Pain" }])
+    }
+    if (req.query.value== '280Original'){
+        res.send([{Nom:"Ognion"},{Nom:"Tomates"},{Nom:"Oeuf" },{Nom:"Steak" },{Nom:"Pain" }])
+    }
+});
 
 app.listen(port, ()=>{
     console.log('Example app listening on port', port);
